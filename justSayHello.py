a7a0b5a178d272417f82272e1ba29023471f2500
Killbot <tharwan1507@gmail.com>
2026-06-18 23:31:52 +0800

    Create justSayHello.py

---
+++ justSayHello.py
@@ -0,0 +1,10 @@
+import ctypes
+
+def Popup(Owner=0, Text="Sample text", Title="Title", Flags=None):
+   if Flags == None: Flags = [0x1, 0x40]
+   return ctypes.windll.user32.MessageBoxW(Owner, Text, Title, Flags[0] | Flags[1]
+
+if __name__ == "__main__":
+   Popup(0, "Hi", "Title here", [0x1, 0x40])
+else:
+   print("No")
